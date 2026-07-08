ccb37e03b2a3858613edb4d65aea88b8ec9e4377
Nguyen-dev1506 <khoinguyenphan2014@gmail.com>
2026-07-08 10:51:55 +0700

    Remove one-off patch scripts used during refactor

--- patch_board_eval.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('src/board.cpp', 'r') as f:
-    content = f.read()
-
-# Delete the PST arrays and update_pst_score in board.cpp
-# I'll just use a python script to strip them or I'll overwrite board.cpp.
-# Actually, I can just include eval.h in board.cpp.
